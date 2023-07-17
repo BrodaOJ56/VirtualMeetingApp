805504bb3add83687925eb60405f22ad2e847cb1
BrodaOJ56 <olujas1@gmail.com>
2023-07-17 22:31:04 +0100

    created models

--- virtual_app/models.py
+++ virtual_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=255)
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    meeting_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Participant(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    joined_at = models.DateTimeField(auto_now_add=True)
+    is_host = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.meeting.title}"
+
+
+class MeetingRecording(models.Model):
+    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    recording_file = models.FileField(upload_to='meeting_recordings/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.meeting.title
