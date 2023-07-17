71f128e2e48464286d192af9ccf16a67e6c4da13
BrodaOJ56 <olujas1@gmail.com>
2023-07-17 23:15:42 +0100

    registered models

--- virtual_app/admin.py
+++ virtual_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Meeting, Participant, MeetingRecording
 # Register your models here.
+
+admin.site.register(Meeting)
+admin.site.register(Participant)
+admin.site.register(MeetingRecording)
