902518067ede2d792a5d0affe950711f735a12f2
BrodaOJ56 <olujas1@gmail.com>
2023-07-17 22:32:44 +0100

    updated INSTALLED_APPS

--- virtual_project/settings.py
+++ virtual_project/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'virtual_app.apps.virtual_appConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
