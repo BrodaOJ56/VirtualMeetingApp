c9ca1e0c9a527e74bba79b9ca1b16700c3307b28
BrodaOJ56 <olujas1@gmail.com>
2023-07-19 06:57:47 +0100

    implemented view logic

--- virtual_app/endpoints/create_meeting.py
+++ virtual_app/endpoints/create_meeting.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from ..models import Meeting
 
 def create_meeting(request):
-    # Implement your create meeting view logic here
     if request.method == 'POST':
         # Process form submission and create a new meeting
+        title = request.POST.get('title')
+        start_time = request.POST.get('start_time')
+        end_time = request.POST.get('end_time')
+        
+        # Perform necessary validation and create the meeting object
+        meeting = Meeting.objects.create(title=title, start_time=start_time, end_time=end_time)
+        
         # Redirect to the meeting detail page or any other desired page
-        pass
-    else:
-        # Render the create meeting form template
-        return render(request, 'virtual_app/create_meeting.html')
+        return redirect('virtual_app:meeting_detail', meeting_id=meeting.id)
+    
+    # Render the create meeting form template
+    return render(request, 'virtual_app/create_meeting.html')
