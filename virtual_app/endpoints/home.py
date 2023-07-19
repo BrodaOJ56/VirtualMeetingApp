2b5565c7fafcfb69645c7efdee8fc91ed6c7a33b
BrodaOJ56 <olujas1@gmail.com>
2023-07-19 06:57:55 +0100

    implemented view logic

--- virtual_app/endpoints/home.py
+++ virtual_app/endpoints/home.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    # Implement your home view logic here
-    return render(request, 'virtual_app/home.html')
+    # Get any necessary data or perform calculations
+    data = {
+        'message': 'Welcome to the Virtual App!'
+    }
+    
+    # Render the template with the data
+    return render(request, 'virtual_app/home.html', context=data)
