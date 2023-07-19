33c09d5e1bc6b74a812d36c836917adcbe33987d
BrodaOJ56 <olujas1@gmail.com>
2023-07-19 06:58:50 +0100

    implemented view logic

--- virtual_app/endpoints/meeting_detail.py
+++ virtual_app/endpoints/meeting_detail.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from ..models import Meeting
 
 def meeting_detail(request, meeting_id):
     # Get the meeting object or return a 404 error if not found
     meeting = get_object_or_404(Meeting, id=meeting_id)
     
-    # Implement other meeting detail view logic here
-    return render(request, 'virtual_app/meeting_detail.html', {'meeting': meeting})
+    # check if the meeting has already started or ended
+    current_time = timezone.now()
+    has_started = meeting.start_time <= current_time
+    has_ended = meeting.end_time <= current_time
+    
+    return render(request, 'virtual_app/meeting_detail.html', {
+        'meeting': meeting,
+        'has_started': has_started,
+        'has_ended': has_ended,
+    })
