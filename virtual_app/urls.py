9c375ec373e807cb225b8139c7b1ea375166f473
BrodaOJ56 <olujas1@gmail.com>
2023-07-20 08:03:48 +0100

    updated url paths

--- virtual_app/urls.py
+++ virtual_app/urls.py
@@ -1,23 +1,29 @@
 from django.urls import path
-from . import views
+from .endpoints.home import home
+from .endpoints.create_meeting import create_meeting
+from .endpoints.join_meeting import join_meeting
+from .endpoints.list_participants import list_participants
+from .endpoints.list_recordings import list_recordings
+from .endpoints.meeting_detail import meeting_detail
+from .endpoints.profile import profile
 
 app_name = 'virtual_app'
 
 urlpatterns = [
     # Home page
-    path('', views.home, name='home'),
+    path('', home, name='home'),
 
     # Meeting related URLs
-    path('meeting/create/', views.create_meeting, name='create_meeting'),
-    path('meeting/<int:meeting_id>/', views.meeting_detail, name='meeting_detail'),
-    path('meeting/<int:meeting_id>/join/', views.join_meeting, name='join_meeting'),
+    path('meeting/create/', create_meeting, name='create_meeting'),
+    path('meeting/<int:meeting_id>/', meeting_detail, name='meeting_detail'),
+    path('meeting/<int:meeting_id>/join/', join_meeting, name='join_meeting'),
 
     # Participant related URLs
-    path('meeting/<int:meeting_id>/participants/', views.list_participants, name='list_participants'),
+    path('meeting/<int:meeting_id>/participants/', list_participants, name='list_participants'),
 
     # Recording related URLs
-    path('meeting/<int:meeting_id>/recordings/', views.list_recordings, name='list_recordings'),
+    path('meeting/<int:meeting_id>/recordings/', list_recordings, name='list_recordings'),
 
     # User related URLs (for example, profile)
-    path('profile/', views.profile, name='profile'),
+    path('profile/', profile, name='profile'),
 ]
