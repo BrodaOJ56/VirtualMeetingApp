88d4097f445ebe859db530a527c6545ec48f63a4
BrodaOJ56 <olujas1@gmail.com>
2023-07-26 06:27:59 +0100

    Update the Meeting model

--- virtual_app/models.py
+++ virtual_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.crypto import get_random_string
 
 class Meeting(models.Model):
     title = models.CharField(max_length=255)
@@ -9,7 +9,17 @@ class Meeting(models.Model):
     end_time = models.DateTimeField()
     meeting_url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
+    invitation_link = models.CharField(max_length=50, unique=True, blank=True, null=True)
 
+
+
+    def save(self, *args, **kwargs):
+        if not self.invitation_link:
+            # Generate a unique invitation link for the meeting
+            self.invitation_link = get_random_string(length=20)  # You can adjust the length as needed
+        super().save(*args, **kwargs)
+
+        
     def __str__(self):
         return self.title
 
