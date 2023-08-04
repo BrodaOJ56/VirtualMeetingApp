4a71fc34c5aa66c8722f9b9c05b215ce750f7197
BrodaOJ56 <olujas1@gmail.com>
2023-08-04 07:02:06 +0100

    updated

--- virtual_project/urls.py
+++ virtual_project/urls.py
@@ -1,22 +1,8 @@
-"""
-URL configuration for virtual_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from django.urls import path, include
+from django.urls import path, re_path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.views import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +10,14 @@ urlpatterns = [
     path('', include('virtual_app.urls')),
     path('accounts/', include('accounts.urls')),
 ]
+
+# Serve static files in production using Django's static() function
+if settings.DEBUG:
+    # In development, serve static files through Django for simplicity
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # In production, configure your web server to serve static files
+    # and media files. The urlpatterns here are for media files.
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
